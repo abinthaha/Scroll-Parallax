9187cc8085b5d24aa530ab5b9ae67e1113e008b5
Abin Thaha <abin@qburst.com>
2016-05-24 20:11:04 +0530

    ScrollSpy implemented

---
+++ js/script.js
@@ -0,0 +1,31 @@
+var TopPosArray = [];
+$(document).ready(function () {
+	var body = $("html, body"),
+		st = $(document).scrollTop(),
+		flag = true;
+
+	$('.contents section').each(function(item) {
+		TopPosArray.push($(this).offset().top);
+		body.animate({scrollTop:1}, '2000', 'swing', function() {});
+	});
+
+	$(document).scroll(function (event) {
+		var current = $('#navigator .active'),
+			index = current.index(),
+			st = $(document).scrollTop();
+		if (st > TopPosArray[index+1]) {
+			current.removeClass('active');
+			current.next().addClass("active");
+		}
+		else if (st < TopPosArray[index]) {
+			current.removeClass('active');
+			current.prev().addClass("active");
+		}		
+	});
+
+	$('#navigator').on('click', 'li', function (ev) {
+		var index = $(this).index();
+		var top = TopPosArray[index];
+		body.animate({scrollTop:top+1}, '2000', 'swing', function() {});
+	});
+});
