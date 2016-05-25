6982fb19caad52e227d78e64a353b4fec9dcce36
Abin Thaha <abin@qburst.com>
2016-05-25 10:25:17 +0530

    Scroll - Partial Commit

--- js/script.js
+++ js/script.js
@@ -1,18 +1,35 @@
 var TopPosArray = [];
 $(document).ready(function () {
-	var body = $("html, body"),
+	var body = $("body"),
 		st = $(document).scrollTop(),
-		flag = true;
+		flag = true,
+		oldPos = 0;
 
 	$('.contents section').each(function(item) {
 		TopPosArray.push($(this).offset().top);
-		body.animate({scrollTop:1}, '2000', 'swing', function() {});
+		body.animate({scrollTop:1}, function() {});
 	});
 
 	$(document).scroll(function (event) {
 		var current = $('#navigator .active'),
 			index = current.index(),
 			st = $(document).scrollTop();
+
+		// if (st > oldPos) {
+		// 	oldPos = st;
+		// 	if (st%768 > 192) {
+		// 		var topVal = TopPosArray[index+1];
+		// 		body.animate({scrollTop:topVal+1}, function() {});
+		// 	}
+		// }
+		// else {
+		// 	oldPos = st;
+		// 	if (st%768 > 192) {
+		// 		var topVal = TopPosArray[index];
+		// 		body.animate({scrollTop:topVal+1}, function() {});
+		// 	}
+		// }
+
 		if (st > TopPosArray[index+1]) {
 			current.removeClass('active');
 			current.next().addClass("active");
@@ -26,6 +43,6 @@ $(document).ready(function () {
 	$('#navigator').on('click', 'li', function (ev) {
 		var index = $(this).index();
 		var top = TopPosArray[index];
-		body.animate({scrollTop:top+1}, '2000', 'swing', function() {});
+		body.animate({scrollTop:top+1}, function() {});
 	});
 });
